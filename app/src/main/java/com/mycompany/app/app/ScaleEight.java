0efb283ab24f44fc78593b36d0ad53a23fcc1fbc
EdoardoDini <edoardodini94@gmail.com>
2021-04-02 16:27:04 +0200

    Modified ScaleEight and ScaleCreator

--- app/src/main/java/com/mycompany/app/app/ScaleEight.java
+++ app/src/main/java/com/mycompany/app/app/ScaleEight.java
@@ -13,6 +13,8 @@ public class ScaleEight implements Sequence {
 	public ScaleEight(List<Interval> scaleIntervals) {
 		if (scaleIntervals.size()==7) {
 			if (checkIntervals(scaleIntervals)) {
+				this.scaleIntervals = scaleIntervals;
+				LOGGER.debug("created a scale by setting some intervals");
 				LOGGER.info(() -> String.format("created a eight scale by setting some intervals"));
 			} else {
 				LOGGER.debug(() -> String.format("New eight scale not created: invalid arguments"));
@@ -22,23 +24,27 @@ public class ScaleEight implements Sequence {
 			LOGGER.debug(() -> String.format("New eight scale not created: invalid arguments"));
 			throw new IllegalArgumentException("The argument should contain 7 intervals");
 		}
-		
-		this.scaleIntervals = scaleIntervals;
-		LOGGER.debug("created a scale by setting some intervals");
 	}
 	
 	private boolean checkIntervals(List<Interval> intList) {
 		int tones = 0;
 		int halfs = 0;
+		int firstHalf = 0;
+		int secondHalf = 0;
 		for (int i=0;i<intList.size();i++) {
 			if (intList.get(i).getIntervalTones()==1 && intList.get(i).getIntervalHalfTones()==0) {
 				tones=tones+1;
 			}
 			if (intList.get(i).getIntervalTones()==0 && intList.get(i).getIntervalHalfTones()==1) {
+				if(halfs==0) {
+					firstHalf = i;
+				}else {
+					secondHalf = i;
+				}
 				halfs=halfs+1;
 			}
 		}
-		if (tones==5 && halfs ==2) {
+		if (tones==5 && halfs ==2 && (secondHalf-firstHalf==3||secondHalf-firstHalf==4)) {
 			return true;
 		}else {
 			return false;
