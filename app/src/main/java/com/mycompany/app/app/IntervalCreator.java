f81419c56f28cf1d4951850ef1ba58df3a2e5c8a
EdoardoDini <edoardodini94@gmail.com>
2021-04-20 00:58:10 +0200

    IntervalCreator code smells removed

--- app/src/main/java/com/mycompany/app/app/IntervalCreator.java
+++ app/src/main/java/com/mycompany/app/app/IntervalCreator.java
@@ -5,9 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 public class IntervalCreator {
 	private static final Logger LOGGER = LogManager.getLogger(IntervalCreator.class);
-	private Note[] allNotes = { new Note("DO"), new Note("DO#"), new Note("RE"), new Note("RE#"), new Note("MI"),new Note("FA"), new Note("FA#"), new Note("SOL"), new Note("SOL#"), new Note("LA"), new Note("LA#"),new Note("SI") };
-
-	// private NoteComparator noteComp = new NoteComparator();
+	private Note[] allNotes = { new Note("DO"), new Note("DO#"), new Note("RE"), new Note("RE#"), new Note("MI"),
+			new Note("FA"), new Note("FA#"), new Note("SOL"), new Note("SOL#"), new Note("LA"), new Note("LA#"),
+			new Note("SI") };
 
 	public Note getIntervalUp(Note note, Interval interval) {
 		int i = 0;
@@ -16,7 +16,8 @@ public class IntervalCreator {
 			i++;
 		}
 		noteToBeReturned = allNotes[(i + (interval.getIntervalTones() * 2) + interval.getIntervalHalfTones()) % 12];
-		LOGGER.debug(() -> String.format("Asked the note far from %s %d tones and %d half tones: %s", note.getNote(),interval.getIntervalTones(), interval.getIntervalHalfTones(), noteToBeReturned.getNote()));
+		LOGGER.debug(() -> String.format("Asked the note far from %s %d tones and %d half tones: %s", note.getNote(),
+				interval.getIntervalTones(), interval.getIntervalHalfTones(), noteToBeReturned.getNote()));
 		return noteToBeReturned;
 	}
 
